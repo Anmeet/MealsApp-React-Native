3f24eb685fb763ee1380174030c7c2da2a5b0aa1
Amit Bhandari <anmeet619@yahoo.com>
2023-01-16 17:03:07 +1100

    Added all files

---
+++ screen/MealDetailScreen.js
@@ -0,0 +1,107 @@
+import { useContext, useLayoutEffect } from 'react'
+import { View, Text, Image, StyleSheet, ScrollView } from 'react-native'
+import { useDispatch, useSelector } from 'react-redux'
+import { addFavorite, removeFavorite } from '../store/redux/favorites'
+
+import IconButton from '../components/IconButton'
+import List from '../components/MealDetail/List'
+import Subtitle from '../components/MealDetail/Subtitle'
+import MealDetails from '../components/MealDetails'
+import { MEALS } from '../data/dummy-data'
+//import { FavoritesContext } from '../store/context/favorites-context'
+
+function MealDetailScreen({ route, navigation }) {
+  //const favoriteMealCtx = useContext(FavoritesContext)
+  const mealId = route.params.mealId
+  const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids)
+  const dispatch = useDispatch()
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId)
+
+  //const mealIsFavorite = favoriteMealCtx.ids.includes(mealId)
+  const mealIsFavorite = favoriteMealIds.includes(mealId)
+
+  function changeFavoriteStatusHandler() {
+    // if (mealIsFavorite) {
+    //   favoriteMealCtx.removeFavorite(mealId)
+    // } else {
+    //   favoriteMealCtx.addFavorite(mealId)
+    // }
+    if (mealIsFavorite) {
+      dispatch(
+        removeFavorite({
+          id: mealId,
+        })
+      )
+    } else {
+      dispatch(
+        addFavorite({
+          id: mealId,
+        })
+      )
+    }
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <IconButton
+            icon={mealIsFavorite ? 'star' : 'star-outline'}
+            color='white'
+            onPress={changeFavoriteStatusHandler}
+          />
+        )
+      },
+    })
+  }, [navigation, changeFavoriteStatusHandler])
+
+  return (
+    <ScrollView style={styles.rootContainer}>
+      <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+        textStyle={styles.detailText}
+      />
+      <View style={styles.listOuterContainer}>
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={selectedMeal.ingredients} />
+          <Subtitle>Steps</Subtitle>
+          <List data={selectedMeal.steps} />
+        </View>
+      </View>
+    </ScrollView>
+  )
+}
+
+export default MealDetailScreen
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    marginBottom: 32,
+  },
+  image: {
+    width: '100%',
+    height: 350,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    margin: 8,
+    textAlign: 'center',
+    color: 'white',
+  },
+  detailText: {
+    color: 'white',
+  },
+  listOuterContainer: {
+    alignItems: 'center',
+  },
+  listContainer: {
+    width: '80%',
+  },
+})
